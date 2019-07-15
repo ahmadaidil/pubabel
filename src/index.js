35ff7644faeaffd6a3730b29e3b44bdbb2223f2b
Ahmad Aidil F <ahmdaidil@gmail.com>
2019-07-15 10:35:47 +0700

    add simple button

--- src/index.js
+++ src/index.js
@@ -1,38 +1,19 @@
-import React from 'react'
-import RD from 'react-dom'
-import sc from 'styled-components'
+import React from "react";
+import RD from "react-dom";
+import sc from "styled-components";
 
-function hex2rgba(hex, alpha) {
-   hex   = hex.replace('#', '');
-   var r = parseInt(hex.length === 3 ? hex.slice(0, 1).repeat(2) : hex.slice(0, 2), 16);
-   var g = parseInt(hex.length === 3 ? hex.slice(1, 2).repeat(2) : hex.slice(2, 4), 16);
-   var b = parseInt(hex.length === 3 ? hex.slice(2, 3).repeat(2) : hex.slice(4, 6), 16);
-   if ( alpha ) {
-      return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
-   }
-   else {
-      return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-   }
-}
+import Button from "./button";
 
-const Button = sc.button`
-  background: ${p => p.background || '#000'};
-  outline: none;
-  border: 0;
-  font-size: 100%;
-  color: ${p => p.color || '#fff'};
-  cursor: pointer;
-  text-decoration: none;
-  line-height: 1.2;
-  padding: 0.5rem 1rem;
-  border-radius: 0.25rem;
-  white-space: no-wrap;
-  margin: 0.25rem;
+const Container = sc.div`
+   display: flex;
+   flex-direction: column;
+`;
 
-  :hover {
-    background: ${p => hex2rgba(p.background || '#000', 0.5)};
-    color: ${p => hex2rgba(p.color || '#fff', 0.5)};
-  }
-`
+const Index = () => (
+  <Container>
+    <Button>Hehe</Button>
+    <Button>Hehe</Button>
+  </Container>
+);
 
-RD.render(<Button>Ubah data</Button>, document.getElementById('app'))
+RD.render(<Index />, document.getElementById("app"));
